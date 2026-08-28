eee412475ec719507092706ebf53858ca2e8981d
Charan Nihaal R <2301721083013@mcc.edu.in>
2026-08-28 11:53:40 +0530

    ci: configure project path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,1 @@
-import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react'; export default defineConfig({plugins:[react()]});
-
+import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react'; export default defineConfig({base:'/phish-hook/',plugins:[react()]});
